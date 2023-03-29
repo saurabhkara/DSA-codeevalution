e2b8a62bedc0c68455f48b77d01dfc468f0a5037
Saurabh <saurabkumar679@gmail..com>
2023-03-29 18:13:38 +0530

    improved

--- fibonacci.js
+++ fibonacci.js
@@ -1,18 +1,15 @@
 //Fibonacci sequence is a sequence in which each number is sum of two preceding ones.
 
 function fibonacci(n){
-    let n1=0;
-    let n2=1;
-    let output= `${n1}` +" "+` ${n2}`+ " ";
-    for(let i =0; i<n-2; i++){
-        let n3 = n1 + n2;
-        output += n3 +" "
-        n1=n2;
-        n2=n3;
+    let fibb = [0,1]
+    for(let i =2; i<n; i++){
+        let n3 = fibb[i-2] + fibb[i-1];
+        fibb.push(n3);
     }
-    console.log(output);
+    console.log(fibb);
+    return fibb;
 }
 
-fibonacci(7);
+fibonacci(8);
 
 //time complexity is O(n)
