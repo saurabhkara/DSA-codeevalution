343463f8f7170ef91e8d74018d2f2ff1865ccba7
Saurabh <saurabkumar679@gmail..com>
2023-05-07 22:12:05 +0530

    Set done

---
+++ 19map.js
@@ -0,0 +1,7 @@
+const map = new Map([['a',1], ['b',2]]);
+
+map.set(['c',3])
+
+console.log(map);
+console.log(map.has('b'));
+
---
+++ 20Set.js
@@ -0,0 +1,4 @@
+const set = new Set([1,1,2,3,4,5,6,6]);
+set.add(8)
+set.delete(2);
+console.log(set);
