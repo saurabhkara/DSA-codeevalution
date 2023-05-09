26a0ddc1f6ac66f9bf021cd2431fb3e369f0f507
Saurabh <saurabkumar679@gmail..com>
2023-05-09 07:10:32 +0530

    stack basic program done

--- 21Stack.js
+++ 21Stack.js
@@ -36,3 +36,74 @@ stack.print();
 stack.pop();
 stack.isEmpty();
 stack.print();
+
+
+class Stack1{
+    size;
+    arr;
+    top;
+    constructor(){
+        this.size= 5;
+        this.arr =[];
+        this.top=-1
+    }
+    push(elem){
+        if(this.arr.length ===this.size){
+            console.log('Stack is overflow');
+            return
+        }else{
+            this.top=this.top+1;
+            this.arr[this.top]=elem;
+        }
+    }
+
+    pop(){
+        if(this.top===-1){
+            console.log('Stack is underflow');
+            return 
+        }else{
+            return this.arr[this.top--];
+        }
+    }
+
+    peek(){
+        if(this.top>-1){
+            console.log('Top element of Stack is ',this.arr[this.top])
+        return this.arr[this.top];
+        }else{
+            console.log('Nothing is isnide stack');
+        }
+    }
+    
+    stackSize(){
+        console.log('Size of stack is',this.top+1);
+    }
+
+    print(){
+        if(this.top>-1){
+            console.log('Stack ', this.arr.toString())
+        }else{
+            console.log('Nothing is isnide stack');
+        }
+    }
+
+}
+
+const stack1 = new Stack1();
+stack1.push(4)
+stack1.push(5)
+stack1.push(1)
+stack1.push(2)
+stack1.push(3)
+stack1.print();
+stack1.push(6);
+stack1.pop();
+stack1.pop()
+stack1.pop()
+stack1.pop()
+stack1.pop()
+stack1.pop()
+stack1.pop()
+stack1.peek();
+stack1.print();
+stack1.stackSize();
